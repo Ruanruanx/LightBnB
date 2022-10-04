c292d0962915ed98a174ef1621a4da0b3e74bbeb
Ruanruanx <zxin7@uwo.ca>
2022-10-04 21:43:25 +0000

    Search my reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -88,7 +88,29 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT
+  reservations.id,
+  title,
+  reservations.start_date,
+  cost_per_night,
+  avg(property_reviews.rating) as average_rating
+FROM
+  properties
+  JOIN reservations ON property_id = properties.id
+  JOIN users ON users.id = reservations.guest_id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE
+  users.id = $1
+GROUP BY
+  properties.id,reservations.id
+ORDER BY
+  start_date
+LIMIT
+  $2;`,[guest_id,limit])
+  .then((result)=>{
+    return result.rows})
+  
 }
 exports.getAllReservations = getAllReservations;
 
